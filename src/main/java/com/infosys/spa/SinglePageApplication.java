f8fa4241f3d8059b5c1023a6cf6c0a004a63ba47
Brandon Learned <90928439+blearned92@users.noreply.github.com>
2022-11-15 15:40:03 -0700

    Made Backend Accessible to Front End

--- src/main/java/com/infosys/spa/SinglePageApplication.java
+++ src/main/java/com/infosys/spa/SinglePageApplication.java
@@ -2,6 +2,9 @@ package com.infosys.spa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SinglePageApplication {
@@ -10,4 +13,19 @@ public class SinglePageApplication {
 		SpringApplication.run(SinglePageApplication.class, args);
 	}
 
+	@Bean
+	WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedOrigins(
+					"http://localhost:3000"
+				)
+				.allowedMethods("DELETE", "GET", "HEAD", "OPTIONS", "POST", "PUT")
+				.allowCredentials(true);
+			}
+		};
+	}
+	
 }
